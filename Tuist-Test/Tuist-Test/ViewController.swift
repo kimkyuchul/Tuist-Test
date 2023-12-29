bd35e96c3d0ac537b5244bf8e534dc3c3362100a
김규철 <rbcjf122@gmail.com>
2023-12-29 22:42:12 +0900

    init

---
+++ Tuist-Test/Tuist-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tuist-Test
+//
+//  Created by 김규철 on 2023/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
