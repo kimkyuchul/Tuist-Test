45d7117ff1d3fb608501cfd4945f6b08f52bad99
김규철 <rbcjf122@gmail.com>
2024-01-03 01:49:32 +0900

    AppDelegate, AppDelegate 추가해서 앱 실행해보기

---
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  App
+//
+//  Created by 김규철 on 2024/01/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+
+        let vc = UIViewController()
+        vc.view.backgroundColor = .systemPink
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
--- Projects/App/Sources/test.swift
+++
@@ -1,8 +0,0 @@
-//
-//  test.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 김규철 on 2024/01/01.
-//
-
-import Foundation
